eba3d33243a83b91103385d0b9567fbe6a6097c0
yashovardhan99 <yashovardhandhanania@outlook.in>
2018-06-27 00:11:25 +0530

    Trying to add multiChoiceListener to main recycler (NOT WORKING)

--- app/src/main/java/com/yashovardhan99/healersdiary/Adapters/MainListAdapter.java
+++ app/src/main/java/com/yashovardhan99/healersdiary/Adapters/MainListAdapter.java
@@ -3,11 +3,18 @@ package com.yashovardhan99.healersdiary.Adapters;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -23,22 +30,81 @@ import java.util.ArrayList;
 public class MainListAdapter extends RecyclerView.Adapter<MainListAdapter.ViewHolder> {
 
     private static ArrayList<Patient> patientList;
+    private static ArrayList<Patient> selectedPatients;
     //patient array list
     @SuppressLint("StaticFieldLeak")
     private static Context context;
+    static ActionMode mActionMode;
 
     static class ViewHolder extends RecyclerView.ViewHolder {
         TextView mTextView;
-        ViewHolder(RelativeLayout v){
+        ViewHolder(final RelativeLayout v){
             super(v);
+            selectedPatients = new ArrayList<>();
             mTextView = v.findViewById(R.id.patientName);
+
+            final AbsListView.MultiChoiceModeListener actionModeCallbacks = new AbsListView.MultiChoiceModeListener() {
+                @Override
+                public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                    if(checked)
+                        selectedPatients.add(patientList.get(position));
+                    else
+                        selectedPatients.remove(patientList.get(position));
+                    if(selectedPatients.isEmpty())
+                        mActionMode.finish();
+                }
+
+                @Override
+                public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                    menu.add("Delete");
+                    return true;
+                }
+
+                @Override
+                public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                    return false;
+                }
+
+                @Override
+                public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                    return false;
+                }
+
+                @Override
+                public void onDestroyActionMode(ActionMode mode) {
+                    mActionMode = null;
+                }
+            };
             v.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    if(mActionMode!=null) {
+                        if(v.isSelected()){
+                            v.setSelected(false);
+                            v.setBackgroundColor(Color.WHITE);
+                            return;
+                        }
+                        v.setBackgroundColor(Color.LTGRAY);
+                        v.setSelected(true);
+                        return;
+                    }
                     //start patient detail
                     Intent detail = new Intent(context, PatientView.class);
-                    detail.putExtra("PATIENT_UID",patientList.get(getAdapterPosition()).getUid());
+                    detail.putExtra("PATIENT_UID", patientList.get(getAdapterPosition()).getUid());
                     context.startActivity(detail);
+                }
+            });
+            v.setOnLongClickListener(new View.OnLongClickListener()
+            {
+                @Override
+                public boolean onLongClick (View view){
+                    if(mActionMode!=null)
+                        return false;
+                    mActionMode = ((AppCompatActivity) view.getContext()).startActionMode(actionModeCallbacks);
+                    Log.d("ACTION_MODE",mActionMode.toString());
+                    view.setSelected(true);
+                    view.setBackgroundColor(Color.LTGRAY);
+                    return true;
                 }
             });
         }
@@ -71,4 +137,5 @@ public class MainListAdapter extends RecyclerView.Adapter<MainListAdapter.ViewHo
         else
             return 0;
     }
+
 }
