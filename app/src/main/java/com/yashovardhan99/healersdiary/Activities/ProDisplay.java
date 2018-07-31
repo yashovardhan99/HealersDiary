8a0de74a61c39104b9645c2b8fa0926dffbdb4b7
yashovardhan99 <yashovardhandhanania@outlook.in>
2018-07-31 16:56:14 +0530

    added pro activity. to be ccompleted
    Signed-off-by: yashovardhan99 <yashovardhandhanania@outlook.in>

---
+++ app/src/main/java/com/yashovardhan99/healersdiary/Activities/ProDisplay.java
@@ -0,0 +1,15 @@
+package com.yashovardhan99.healersdiary.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.yashovardhan99.healersdiary.R;
+
+public class ProDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pro_display);
+    }
+}
