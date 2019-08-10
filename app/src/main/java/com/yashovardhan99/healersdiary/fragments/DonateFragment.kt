7c3f6387a3430a2ced59e364418ee8e7f924274b
yashovardhan99 <yashovardhandhanania@outlook.in>
2019-08-10 23:00:02 +0530

    Added Donate via paypal option
    Signed-off-by: yashovardhan99 <yashovardhandhanania@outlook.in>

--- app/src/main/java/com/yashovardhan99/healersdiary/fragments/DonateFragment.kt
+++ app/src/main/java/com/yashovardhan99/healersdiary/fragments/DonateFragment.kt
@@ -1,5 +1,7 @@
 package com.yashovardhan99.healersdiary.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +9,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
 import com.yashovardhan99.healersdiary.R
 
 /**
@@ -17,6 +20,7 @@ class DonateFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_donate_display, container, false)
         val toolbar = rootView.findViewById<Toolbar>(R.id.toolbar)
+        val donatePaypal = rootView.findViewById<MaterialButton>(R.id.donate_paypal)
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
         val actionBar = (activity as AppCompatActivity).supportActionBar
         if (actionBar != null) {
@@ -24,6 +28,11 @@ class DonateFragment : Fragment() {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
         }
+        donatePaypal.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.paypal_url))))
+        }
         return rootView
     }
+
+
 }
