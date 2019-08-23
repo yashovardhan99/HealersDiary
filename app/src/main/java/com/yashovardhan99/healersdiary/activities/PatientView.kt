9019c1d533720c3dead71e6d0bedd2d34f65f43f
yashovardhan99 <yashovardhandhanania@outlook.in>
2019-08-23 23:58:06 +0530

    Updated gradle
    Signed-off-by: yashovardhan99 <yashovardhandhanania@outlook.in>

--- app/src/main/java/com/yashovardhan99/healersdiary/activities/PatientView.kt
+++ app/src/main/java/com/yashovardhan99/healersdiary/activities/PatientView.kt
@@ -27,7 +27,6 @@ class PatientView : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_view)
-
         setSupportActionBar(findViewById<View>(R.id.toolbar) as Toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.title = "Patient Detail"
