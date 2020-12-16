5502449889b9f16ce740e645bd704bbdc9899376
yashovardhan99 <yashovardhandhanania@gmail.com>
2020-12-16 17:41:18 +0530

    Setting up MainActivity

--- app/src/main/java/com/yashovardhan99/healersdiary/dashboard/MainActivity.kt
+++ app/src/main/java/com/yashovardhan99/healersdiary/dashboard/MainActivity.kt
@@ -2,13 +2,21 @@ package com.yashovardhan99.healersdiary.dashboard
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.findFragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.yashovardhan99.healersdiary.R
+import com.yashovardhan99.healersdiary.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navHostFragment = binding.navHostFragmentContainer.findFragment() as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
