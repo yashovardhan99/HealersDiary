1077f8a38e2b5157414ce42d296ec9e9d5131a29
yashovardhan99 <yashovardhandhanania@gmail.com>
2020-12-17 00:43:47 +0530

    Fixed lint check:

--- app/src/main/java/com/yashovardhan99/healersdiary/database/HealersDao.kt
+++ app/src/main/java/com/yashovardhan99/healersdiary/database/HealersDao.kt
@@ -9,29 +9,29 @@ import kotlinx.coroutines.flow.Flow
 import java.util.*
 
 @Dao
-interface HealersDao {
+abstract class HealersDao {
 
     @Query("SELECT * FROM patients")
-    fun getAllPatients(): Flow<List<Patient>>
+    abstract fun getAllPatients(): Flow<List<Patient>>
 
     @Query("SELECT * FROM healings WHERE time>=:startDate")
-    fun getAllHealings(startDate: Date): Flow<List<Healing>>
+    abstract fun getAllHealings(startDate: Date): Flow<List<Healing>>
 
     @Query("SELECT * FROM payments WHERE time>=:startDate")
-    fun getAllPayments(startDate: Date): Flow<List<Payment>>
+    abstract fun getAllPayments(startDate: Date): Flow<List<Payment>>
 
     @Query("SELECT * FROM healings WHERE patient_id == :patientId ORDER BY time DESC")
-    fun getAllHealings(patientId: Long): PagingSource<Int, Healing>
+    abstract fun getAllHealings(patientId: Long): PagingSource<Int, Healing>
 
     @Query("SELECT * FROM payments WHERE patient_id == :patientId ORDER BY time DESC")
-    fun getAllPayments(patientId: Long): PagingSource<Int, Payment>
+    abstract fun getAllPayments(patientId: Long): PagingSource<Int, Payment>
 
     @Insert(entity = Patient::class, onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertPatient(patient: Patient): Long
+    abstract suspend fun insertPatient(patient: Patient): Long
 
     @Insert(entity = Healing::class, onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertHealing(patient: Healing): Long
+    abstract suspend fun insertHealing(healing: Healing): Long
 
     @Insert(entity = Payment::class, onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertPayment(payment: Payment): Long
+    abstract suspend fun insertPayment(payment: Payment): Long
 }
