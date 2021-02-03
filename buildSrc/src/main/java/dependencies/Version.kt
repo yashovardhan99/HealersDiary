03319005904b9f9375e740cf9f3f88064a46e384
yashovardhan99 <yashovardhandhanania@gmail.com>
2021-02-03 22:13:52 +0530

    Added all dependency versions in kotlin

--- buildSrc/src/main/java/dependencies/Version.kt
+++ buildSrc/src/main/java/dependencies/Version.kt
@@ -38,4 +38,33 @@ object Version {
             object RELEASE : BuildType(3, "release")
         }
     }
+
+    object Dependencies {
+        const val kotlin = "1.4.21"
+        const val material = "1.2.1"
+        const val hilt = "1.0.0-alpha03"
+        const val hilt_android = "2.31.2-alpha"
+        const val timber = "4.7.1"
+        const val gradle = "4.1.2"
+        const val oss_app = "17.0.0"
+        const val oss_root = "0.10.2"
+        const val crashlytics = "2.4.1"
+        const val google_services = "4.3.5"
+        const val play_services_auth = "19.0.0"
+        const val firebase_bom = "26.4.0"
+
+        object Androidx {
+            const val app_compat = "1.2.0"
+            const val constraint_layout = "2.0.4"
+            const val recycler = "1.1.0"
+            const val multidex = "2.0.1"
+            const val ktx_core = "1.3.2"
+            const val lifecycle = "2.2.0"
+            const val navigation = "2.3.3"
+            const val paging = "3.0.0-alpha13"
+            const val datastore = "1.0.0-alpha06"
+            const val activity = "1.2.0-rc01"
+            const val fragment = "1.3.0-rc02"
+        }
+    }
 }
