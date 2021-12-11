efb9e091526544885132307fadc49288abe53686
Yashovardhan Dhanania <yashovardhandhanania@gmail.com>
2021-12-11 20:22:18 +0530

    Remove unused Date converters

--- core/src/main/java/com/yashovardhan99/core/database/Converters.kt
+++ core/src/main/java/com/yashovardhan99/core/database/Converters.kt
@@ -4,18 +4,8 @@ import androidx.room.TypeConverter
 import com.yashovardhan99.core.getLocalDateTimeFromMillis
 import com.yashovardhan99.core.toEpochMilli
 import java.time.LocalDateTime
-import java.util.*
 
 class DateConverter {
-    @TypeConverter
-    fun fromTimestamp(value: Long?): Date? {
-        return value?.let { Date(it) }
-    }
-
-    @TypeConverter
-    fun dateToTimestamp(date: Date?): Long? {
-        return date?.time
-    }
 
     @TypeConverter
     fun timestampToLocalDateTime(value: Long?): LocalDateTime? {
