afaee9f6a56a912cedc948eb6f72827568609ac8
Yashovardhan Dhanania <yashovardhandhanania@gmail.com>
2022-05-18 00:20:03 +0530

    Converted CreateRepository to an interface for easier testing

--- app/src/main/java/com/yashovardhan99/healersdiary/create/CreateRepository.kt
+++ app/src/main/java/com/yashovardhan99/healersdiary/create/CreateRepository.kt
@@ -5,18 +5,35 @@ import com.yashovardhan99.core.database.Healing
 import com.yashovardhan99.core.database.Patient
 import com.yashovardhan99.core.database.Payment
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class CreateRepository @Inject constructor(private val healersDao: HealersDao) {
-    suspend fun insertNewHealing(healing: Healing) = healersDao.newHealing(healing)
-    suspend fun insertNewPayment(payment: Payment) = healersDao.newPayment(payment)
-    suspend fun insertNewPatient(patient: Patient) = healersDao.insertPatient(patient)
-    suspend fun getPatient(patientId: Long): Patient? = healersDao.getPatient(patientId)
-    suspend fun updatePatient(updatedPatient: Patient) = healersDao.updatePatient(updatedPatient)
-    suspend fun deletePatient(patient: Patient) = healersDao.deletePatientData(patient)
-    suspend fun getHealing(healingId: Long): Healing? = healersDao.getHealing(healingId)
-    suspend fun getPayment(paymentId: Long): Payment? = healersDao.getPayment(paymentId)
-    suspend fun updateHealing(oldHealing: Healing, newHealing: Healing) = healersDao.updateHealing(oldHealing, newHealing)
-    suspend fun updatePayment(oldPayment: Payment, newPayment: Payment) = healersDao.updatePayment(oldPayment, newPayment)
+interface CreateRepository {
+    suspend fun insertNewHealing(healing: Healing): Long
+    suspend fun insertNewPayment(payment: Payment): Long
+    suspend fun insertNewPatient(patient: Patient): Long
+    suspend fun getPatient(patientId: Long): Patient?
+    suspend fun updatePatient(updatedPatient: Patient)
+    suspend fun deletePatient(patient: Patient)
+    suspend fun getHealing(healingId: Long): Healing?
+    suspend fun getPayment(paymentId: Long): Payment?
+    suspend fun updateHealing(oldHealing: Healing, newHealing: Healing)
+    suspend fun updatePayment(oldPayment: Payment, newPayment: Payment)
+}
+
+class CreateRepositoryImpl @Inject constructor(private val healersDao: HealersDao) :
+    CreateRepository {
+    override suspend fun insertNewHealing(healing: Healing) = healersDao.newHealing(healing)
+    override suspend fun insertNewPayment(payment: Payment) = healersDao.newPayment(payment)
+    override suspend fun insertNewPatient(patient: Patient) = healersDao.insertPatient(patient)
+    override suspend fun getPatient(patientId: Long): Patient? = healersDao.getPatient(patientId)
+    override suspend fun updatePatient(updatedPatient: Patient) =
+        healersDao.updatePatient(updatedPatient)
+
+    override suspend fun deletePatient(patient: Patient) = healersDao.deletePatientData(patient)
+    override suspend fun getHealing(healingId: Long): Healing? = healersDao.getHealing(healingId)
+    override suspend fun getPayment(paymentId: Long): Payment? = healersDao.getPayment(paymentId)
+    override suspend fun updateHealing(oldHealing: Healing, newHealing: Healing) =
+        healersDao.updateHealing(oldHealing, newHealing)
+
+    override suspend fun updatePayment(oldPayment: Payment, newPayment: Payment) =
+        healersDao.updatePayment(oldPayment, newPayment)
 }
--- app/src/main/java/com/yashovardhan99/healersdiary/utils/HiltModules.kt
+++ app/src/main/java/com/yashovardhan99/healersdiary/utils/HiltModules.kt
@@ -1,5 +1,7 @@
 package com.yashovardhan99.healersdiary.utils
 
+import com.yashovardhan99.healersdiary.create.CreateRepository
+import com.yashovardhan99.healersdiary.create.CreateRepositoryImpl
 import com.yashovardhan99.healersdiary.dashboard.DashboardRepository
 import com.yashovardhan99.healersdiary.dashboard.DashboardRepositoryImpl
 import dagger.Binds
@@ -10,6 +12,12 @@ import dagger.hilt.android.components.ViewModelComponent
 @Module
 @InstallIn(ViewModelComponent::class)
 abstract class RepositoryModule {
+    /**
+     * Instantiates [CreateRepositoryImpl] and injects it as [CreateRepository] into different ViewModels as needed
+     * */
+    @Binds
+    abstract fun bindCreateRepository(createRepositoryImpl: CreateRepositoryImpl): CreateRepository
+
     /**
      * Instantiates [DashboardRepositoryImpl] and injects it as [DashboardRepository] into different ViewModels as needed
      * */
