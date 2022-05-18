3045d782f35ab62b3edb062988482ab2d51a7644
Yashovardhan Dhanania <yashovardhandhanania@gmail.com>
2022-05-18 22:01:02 +0530

    Added logging via Timber for uncaught exceptions

--- app/src/main/java/com/yashovardhan99/healersdiary/HealersApp.kt
+++ app/src/main/java/com/yashovardhan99/healersdiary/HealersApp.kt
@@ -4,14 +4,20 @@ import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import com.google.android.play.core.splitcompat.SplitCompatApplication
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 import timber.log.Timber
+import javax.inject.Inject
+import kotlin.system.exitProcess
 
 @HiltAndroidApp
 class HealersApp : SplitCompatApplication(), Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+
+        Thread.setDefaultUncaughtExceptionHandler { t, e ->
+            Timber.wtf(e, "Uncaught Exception received in Thread $t")
+            exitProcess(1)
+        }
     }
 
     @Inject
